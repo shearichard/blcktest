dc39507e24277950cc3b9af794e7c3fa01cfe68c
Richard Shea <rshea@thecubagroup.com>
2015-01-13 14:02:23 +1300

    Apply module pattern to bltest.js

--- bltst/js/bltest.js
+++ bltst/js/bltest.js
@@ -1,10 +1,71 @@
+var SHEARICHARD = SHEARICHARD || function () { };
+SHEARICHARD.BLCKTEST = function () {
+    //Private vars
+    var jscodekey = "blcktest.code";
+    var blockscodekey = "blcktest.blocks";
+
+    return {
+    //**********************************************************
+    //test for local storage support
+    //**********************************************************
+    supports_html5_storage: function() {
+        try {
+            return 'localStorage' in window && window['localStorage'] !== null;
+        } catch (e) {
+            return false;
+        }
+    },
+    //**********************************************************
+    //save js to local storage      
+    //**********************************************************
+    saveCodeState: function(code) {
+        if (!SHEARICHARD.BLCKTEST.supports_html5_storage()) { return false; }
+        localStorage[jscodekey] = code;
+        return true;
+    },
+    //**********************************************************
+    //retrieve js from local storage      
+    //**********************************************************
+    retrieveCodeState: function(code) {
+        if (!SHEARICHARD.BLCKTEST.supports_html5_storage()) { return false; }
+        if (localStorage.getItem(jscodekey) === null) {
+            return false;
+        }else{
+            code = localStorage[jscodekey];
+            return code;
+        }
+    },
+    //**********************************************************
+    //save blocks to local storage      
+    //**********************************************************
+    saveBlockState: function(blocks) {
+        if (!SHEARICHARD.BLCKTEST.supports_html5_storage()) { return false; }
+        localStorage[blockscodekey] = blocks;
+        return true;
+    },
+    //**********************************************************
+    //retrieve blocks from local storage      
+    //**********************************************************
+    retrieveBlockState: function(){
+        if (!SHEARICHARD.BLCKTEST.supports_html5_storage()) { return false; }
+        if (localStorage.getItem(blockscodekey) === null) {
+            return false;
+        }else{
+            var xml = Blockly.Xml.textToDom(window.localStorage[blockscodekey]);
+            Blockly.Xml.domToWorkspace(Blockly.getMainWorkspace(), xml);
+            return true;
+        }
+    }
+    };
+
+}();
 //**********************************************************
 //Initialise blockly
 //**********************************************************
 function blocklyLoaded(blockly) {
     // Called once Blockly is fully loaded.
     window.Blockly = blockly;
-    retrieveBlockState();
+    SHEARICHARD.BLCKTEST.retrieveBlockState();
     
 }
 window.addEventListener("load",function() { // Wait for the window to finish loading
@@ -14,52 +75,9 @@ window.addEventListener("load",function() { // Wait for the window to finish loa
     function myUpdateFunction() {
         var code = window.Blockly.JavaScript.workspaceToCode();
         document.getElementById('samplecodechunk').innerHTML = code;
-        saveCodeState(code);
+        SHEARICHARD.BLCKTEST.saveCodeState(code);
         var xml = Blockly.Xml.workspaceToDom(Blockly.getMainWorkspace());
-        saveBlockState(Blockly.Xml.domToText(xml));
+        SHEARICHARD.BLCKTEST.saveBlockState(Blockly.Xml.domToText(xml));
     }
     Blockly.addChangeListener(myUpdateFunction);
 });
-//**********************************************************
-//test for local storage support
-//**********************************************************
-function supports_html5_storage() {
-    try {
-        return 'localStorage' in window && window['localStorage'] !== null;
-    } catch (e) {
-        return false;
-    }
-}
-//**********************************************************
-//save js to local storage      
-//**********************************************************
-function saveCodeState(code) {
-    if (!supports_html5_storage()) { return false; }
-    localStorage["blcktest.code"] = code;
-    return true;
-}
-//**********************************************************
-//retrieve js from local storage      
-//**********************************************************
-function retrieveCodeState(code) {
-    if (!supports_html5_storage()) { return false; }
-    code = localStorage["blcktest.code"];
-    return code;
-}
-//**********************************************************
-//save blocks to local storage      
-//**********************************************************
-function saveBlockState(blocks) {
-    if (!supports_html5_storage()) { return false; }
-    localStorage["blcktest.blocks"] = blocks;
-    return true;
-}
-//**********************************************************
-//retrieve blocks from local storage      
-//**********************************************************
-function retrieveBlockState(){
-    if (!supports_html5_storage()) { return false; }
-    var xml = Blockly.Xml.textToDom(window.localStorage['blcktest.blocks']);
-    Blockly.Xml.domToWorkspace(Blockly.getMainWorkspace(), xml);
-    return true;
-}
